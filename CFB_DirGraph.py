c516b11f2fc56839ec6f6a9f23fd6ef5aae18f90
Anthony <ant21san@gmail.com>
2021-10-05 00:40:13 -0500

    Use NetGraph Library to make interactive graph. Need to fix resolution.
    Signed-off-by: Anthony <ant21san@gmail.com>

--- CFB_DirGraph.py
+++ CFB_DirGraph.py
@@ -3,6 +3,7 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
+from netgraph import InteractiveGraph # pip install netgraph
 
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
@@ -23,7 +24,7 @@ f = open('completedata.json')
 completedata = json.load(f)
 #len(completedata)
 
-for i in range(0,8):
+for i in range(0,30):
 	temp = completedata[i]
 	team = completedata[i]["team"]
 	opponents = completedata[i]["opponents"]
@@ -49,8 +50,21 @@ G.add_edges_from( gameResults )
 
 
 #really gross right now needs to be fixed.
-pos = nx.spring_layout(G, k=0.5, iterations=50, scale = 3)
-nx.draw_networkx_nodes(G, pos, node_size = 30)
-nx.draw_networkx_edges(G, pos, edgelist = G.edges(), edge_color = 'black')
-nx.draw_networkx_labels(G, pos, font_size = 12)
+#pos = nx.spring_layout(G)#, k=0.5, iterations=50, scale = 3)
+#nx.draw_networkx_nodes(G, pos, node_size = 50)
+#nx.draw_networkx_edges(G, pos, edgelist = G.edges(), edge_color = 'black')
+#nx.draw_networkx_labels(G, pos, font_size = 8)
+#g = InteractiveGraph(nx.complete_graph(10), node_size=2, edge_width=0.5,
+#                     node_labels=dict(zip(range(10), 'abcdefghij')), node_label_offset=0.05,
+#                     node_label_fontdict=dict(size=20, fontweight='bold'))
+#plt.show()
+G = InteractiveGraph(G, 
+					node_size=0.75,
+					edge_width = 0.25, 
+					arrows=True, 
+					node_labels=True, 
+					node_label_offset=0.01, 
+					node_label_fontdict=dict(size=8)) 
+#px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+#G.subplots(figsize=(1920*px, 1080*px))
 plt.show()
